06d43a04a3cb3293c0a24b3895950708352ffcaa
MenonShyama <96658329+MenonShyama@users.noreply.github.com>
2022-01-20 12:28:16 +0530

    Add files via upload

---
+++ NegativeToPositiveNumber.java
@@ -0,0 +1,17 @@
+package mylearnings;
+
+public class NegativeToPositiveNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=-40;
+		int j=0;
+		if(i<0)
+		{
+			j=i*-1;
+		}
+		System.out.println("The number "+i + " is converted to " +j);
+		}
+
+	}
+
